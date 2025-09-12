12e6a11a19390ddea9bd7e553c3958d30584b6a8
bybuss <kukuhaha729@gmail.com>
2025-09-12 21:41:23 +0300

    added: migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,13 +6,27 @@ import ScheduleController from "./presentation/routes/ScheduleController.ts";
 import { GetScheduleUseCase } from "./domain/usecases/GetScheduleUseCase.ts";
 import { ScheduleRepositoryImpl } from "./data/repositories/ScheduleRepository.ts";
 import { pool } from "./common/connection.ts";
+import knex from 'knex';
+import dotenv from 'dotenv';
 import { UserDao } from "./data/dao/UserDao.ts";
-
 import AuthController from "./presentation/routes/AuthController.ts";
 import { AuthRepositoryImpl } from "./data/repositories/AuthRepositoryImpl.ts";
 
+dotenv.config();
+
 const app = express();
 const PORT = 5000;
+
+const db = knex({
+    client: "pg",
+    connection: {
+        host: process.env.DB_HOST || "",
+        port: parseInt(process.env.DB_PORT || "5432"),
+        user: process.env.DB_USER || "",
+        password: process.env.DB_PASSWORD || "",
+        database: process.env.DB_NAME || ""
+    }
+});
 
 container.registerInstance(UserDao, new UserDao(pool));
 container.registerSingleton("AuthRepository", AuthRepositoryImpl)
@@ -21,8 +35,6 @@ container.registerSingleton(AuthController)
 container.registerSingleton("ScheduleRepository", ScheduleRepositoryImpl); 
 container.registerSingleton(GetScheduleUseCase);
 container.registerSingleton(ScheduleController);
-
-console.log(container);
 
 const router = express.Router();
 
@@ -41,6 +53,14 @@ app.use("/api", router);
 app.post("/login", authController.login.bind(authController));
 app.post("/register", authController.register.bind(authController));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+db.migrate.latest()
+  .then(() => {
+    console.log("Migrated successfully!!!!");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Migrations error: ${error}`);
+    process.exit(1);
+  });
